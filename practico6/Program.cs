79f62d52867568a4467fd2b76f6be4c7be468a76
Santiago Gonzalez <santiriver2009@gmail.com>
2023-07-16 18:20:15 -0300

    Ejercicio 3

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,56 +1,50 @@
-﻿/////// EJERECICIO 2 ///////
-
-int op;
-Console.WriteLine("-------MENU-------");
-Console.WriteLine("1- Sumar");
-Console.WriteLine("2- Restar");
-Console.WriteLine("3- Multiplicar");
-Console.WriteLine("4- Dividir");
+﻿/////// EJERECICIO 3 ///////
+bool resulA;
+double numA;
 do
 {
-    Console.WriteLine("Seleccione la opcion que desea: ");
-    op = Console.Read();
-} while (op > 52 | op <= 48);
-
-bool resulA, resulB;
-int numA, numB;
-Console.WriteLine("Ingrese 2 numeros: ");
-do
-{
-    Console.WriteLine("Ingrese el numero A: ");
-    resulA = int.TryParse(Console.ReadLine(),out numA);
+    Console.WriteLine("Ingrese un numero: ");
+    resulA = double.TryParse(Console.ReadLine(),out numA);
 } while (!resulA);
 
+Console.WriteLine("Valor absoluto de " + numA + ": " + (double)Math.Abs(numA));
+Console.WriteLine("Cuadrado de " + numA + ": " + (double)Math.Pow(numA, 2));
+if (numA >= 0)
+{
+    Console.WriteLine("Raiz de " + numA +  ": " + (double)Math.Sqrt(numA));
+} else
+{
+    Console.WriteLine("Raiz de " + numA +  ": SYNTAX ERROR");
+}
+Console.WriteLine("Seno de " + numA +  ": " + (double)Math.Sin(numA));
+Console.WriteLine("Coseno de " + numA +  ": " + (double)Math.Cos(numA));
+Console.WriteLine("Parte entera de " + numA +  ": " + (int)numA);
+
+bool resul1, resul2;
+double num1, num2;
+Console.WriteLine("Ingrese dos numeros:");
 do
 {
-    Console.WriteLine("Ingrese el numero B: ");
-    resulB = int.TryParse(Console.ReadLine(),out numB);
-} while (!resulB);
+    Console.WriteLine("Ingrese el numero 1: ");
+    resul1 = double.TryParse(Console.ReadLine(),out num1);
+} while (!resul1);
 
-int operacion;
-
-switch (op)
+do
 {
-    case 49:
-        operacion = numA + numB;
-        Console.Write("Resultado: "+operacion);
-        break;
-    case 50:
-        operacion = numA - numB;
-        Console.Write("Resultado: "+operacion);
-        break;
-    case 51:
-        operacion = numA * numB;
-        Console.Write("Resultado: "+operacion);
-        break;
-    case 52:
-        if (numB != 0)
-        {
-            operacion = numA / numB;
-            Console.Write("Resultado: "+operacion);
-        } else
-        {
-            Console.WriteLine("Sintax Error");
-        }
-        break;
+    Console.WriteLine("Ingrese el numero 2: ");
+    resul2 = double.TryParse(Console.ReadLine(),out num2);
+} while (!resul2);
+
+if (num1 == num2)
+{
+    Console.WriteLine("Son iguales");
+} else
+{
+    if (num1 > num2)
+    {
+        Console.WriteLine("El numero 1 es el maximo");
+    } else
+    {
+        Console.WriteLine("El numero 2 es el maximo");
+    }
 }
